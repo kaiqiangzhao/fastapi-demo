3129a8994ce4db6d010c2fc5691b13fc5d755705
kaiqiang.zhao <kaiqiang.zh@gmail.com>
2020-11-16 19:44:20 +0800

    add string length

--- app/models/users.py
+++ app/models/users.py
@@ -11,7 +11,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+    hashed_password = Column(String(length=256))
     is_active = Column(Boolean, default=True)
 
     items = relationship("Item", back_populates="owner")
